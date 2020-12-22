27fb8c6b3c1e58e623d915df25b073b4a1ac957e
DatDonathan <jojo08082003@gmail.com>
2020-12-22 19:52:22 +0100

    Updated to diamond ore

--- src/at/jojokobi/generator/populators/ore/OrePopulator.java
+++ src/at/jojokobi/generator/populators/ore/OrePopulator.java
@@ -19,7 +19,7 @@ public class OrePopulator extends BlockPopulator{
 		ores.add(new Ore(Material.GOLD_ORE, 9, 2, 32));
 		ores.add(new Ore(Material.EMERALD_ORE, 1, 3, 32));
 		ores.add(new Ore(Material.REDSTONE_ORE, 8, 8, 16));
-		ores.add(new Ore(Material.DIAMOND, 9, 1, 16));
+		ores.add(new Ore(Material.DIAMOND_ORE, 9, 1, 16));
 		ores.add(new Ore(Material.LAPIS_ORE, 7, 1, 32));
 	}
 
